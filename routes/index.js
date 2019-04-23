d2af2d63e611ec095fff74d14e27fccae49816ed
Chris Grooms <cgrooms21@aol.com>
2019-04-23 14:15:35 -0500

    Add logout methods

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  
 router.get('/logout', function(req, res){
   console.log(req.session);
   req.logout();
